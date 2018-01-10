c3df6c3938947175ba59fed9fc293193fc7980f6
Alok Omkar <alokomkar.gudikote@gmail.com>
2018-01-10 18:19:57 +0530

    Added more model classes

---
+++ app/src/main/java/com/ninebx/ui/home/calendar/model/Reminders.kt
@@ -0,0 +1,10 @@
+package com.ninebx.ui.home.calendar.model
+
+/**
+ * Created by Alok on 10/01/18.
+ */
+val REMINDER_EveryDay                    = "Every Day"
+val REMINDER_EveryWeek                   = "Every Week"
+val REMINDER_Every2Weeks                 = "Every 2 Weeks"
+val REMINDER_EveryMonth                  = "Every Month"
+val REMINDER_EveryYear                   = "Every Year"
