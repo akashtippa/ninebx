417b78919f839e180fa7db8f2dac374d11664ff8
anibha88 <anibha88@gmail.com>
2018-01-12 11:12:57 +0530

    Jan 12, 1111 hrs

---
+++ app/src/main/java/com/ninebx/ui/home/lists/model/AddedItem.kt
@@ -0,0 +1,12 @@
+package com.ninebx.ui.home.lists.model
+
+/***
+ * Created by TechnoBlogger on 12/01/18.
+ */
+
+class AddedItem {
+    var id: String = ""
+    var strAddedItem: String = ""
+
+
+}
