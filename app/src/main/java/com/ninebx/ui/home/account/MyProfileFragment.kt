4bfba0651272bceceba7cd18cfecabb2042cbc85
anibha88 <anibha88@gmail.com>
2018-01-15 09:34:32 +0530

    Jan 15th Updated With Merged, 0933 hrs

---
+++ app/src/main/java/com/ninebx/ui/home/account/MyProfileFragment.kt
@@ -0,0 +1,28 @@
+package com.ninebx.ui.home.account
+
+import android.os.Bundle
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import com.ninebx.NineBxApplication
+import com.ninebx.R
+import com.ninebx.utility.FragmentBackHelper
+
+/***
+ * Created by TechnoBlogger on 15/01/18.
+ */
+class MyProfileFragment : FragmentBackHelper() {
+
+    override fun onCreateView(inflater: LayoutInflater?, container: ViewGroup?, savedInstanceState: Bundle?): View? {
+        return inflater!!.inflate(R.layout.fragment_my_profile, container, false)
+    }
+
+    override fun onViewCreated(view: View?, savedInstanceState: Bundle?) {
+        super.onViewCreated(view, savedInstanceState)
+    }
+
+    override fun onBackPressed(): Boolean {
+        NineBxApplication.instance.activityInstance!!.hideHomeNShowQuickAdd()
+        return super.onBackPressed()
+    }
+}
