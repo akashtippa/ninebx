0abbb7924dc281c2640d46bbb43b7aec8d07506b
anibha88 <anibha88@gmail.com>
2018-01-22 12:58:55 +0530

    Jan 22nd, 1300 hrs

---
+++ app/src/main/java/com/ninebx/ui/home/account/PermissionData.java
@@ -0,0 +1,44 @@
+package com.ninebx.ui.home.account;
+
+import com.ninebx.R;
+
+/***
+ * Created by TechnoBlogger on 22/01/18.
+ */
+
+public class PermissionData {
+    static Integer[] bgDrawable = {R.drawable.bg_lt_home,
+            R.drawable.bg_lt_travell,
+            R.drawable.bg_lt_contacts,
+            R.drawable.bg_lt_edu,
+            R.drawable.bg_lt_personal,
+            R.drawable.bg_lt_interests,
+            R.drawable.bg_lt_wellness,
+            R.drawable.bg_lt_memories,
+            R.drawable.bg_lt_shopping
+    };
+
+    static Integer[] menuDrawable = {R.drawable.ic_home_icon_home_money,
+            R.drawable.ic_home_icon_travel,
+            R.drawable.ic_home_icon_contacts,
+            R.drawable.ic_home_icon_education_work,
+            R.drawable.ic_home_icon_personal,
+            R.drawable.ic_home_icon_interests,
+            R.drawable.ic_home_icon_wellness,
+            R.drawable.ic_home_icon_memories,
+            R.drawable.ic_home_icon_shopping};
+
+    static String[] menuName = {"Home & Money",
+            "Travel",
+            "Contacts",
+            "Education & Work",
+            "Personal",
+            "Interests",
+            "Wellness",
+            "Memories",
+            "Shopping"};
+
+    static boolean[] chkView = {true, true, true, true, true, true, true, true, true};
+    static boolean[] chkAdd = {true, true, true, true, true, true, true, true, true};
+    static boolean[] chkEdit = {true, true, true, true, true, true, true, true, true};
+}
