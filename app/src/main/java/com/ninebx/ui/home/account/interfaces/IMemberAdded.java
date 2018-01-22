a9995bdf4a71b1bab679fa5b8c9a0776fd3d6c9b
anibha88 <anibha88@gmail.com>
2018-01-22 10:29:47 +0530

    Jan 22nd, 1029 hrs

---
+++ app/src/main/java/com/ninebx/ui/home/account/interfaces/IMemberAdded.java
@@ -0,0 +1,9 @@
+package com.ninebx.ui.home.account.interfaces;
+
+/***
+ * Created by TechnoBlogger on 21/01/18.
+ */
+
+public interface IMemberAdded {
+    void memberAdded(String profileName, String accountHolder, String email, String role);
+}
