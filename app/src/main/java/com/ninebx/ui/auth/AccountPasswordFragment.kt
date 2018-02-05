34b22fcd36888772177b3851a7a611db24d2a017
Alok Omkar <alokomkar.gudikote@gmail.com>
2018-02-05 15:08:57 +0530

    Save user object done

--- app/src/main/java/com/ninebx/ui/auth/AccountPasswordFragment.kt
+++ app/src/main/java/com/ninebx/ui/auth/AccountPasswordFragment.kt
@@ -63,7 +63,7 @@ class AccountPasswordFragment : BaseAuthFragment() {
     fun onSuccess(syncUser: SyncUser?) {
         mSyncUser = syncUser
 
-        prepareRealmConnections( context, true, mSyncUser!!.identity + "/Users", object : Realm.Callback() {
+        prepareRealmConnections( context, true, "/Users", object : Realm.Callback() {
             override fun onSuccess(realm: Realm?) {
                 mCurrentUser.insertOrUpdate( realm!! )
                 NineBxApplication.getPreferences().setCurrentUser( mCurrentUser )
