9c2f352424187ba91e2dcb1eaaa94690649741b9
Alok Omkar <alokomkar.gudikote@gmail.com>
2018-02-14 18:24:27 +0530

    Added resend otp with time out option

--- app/src/main/java/com/ninebx/NineBxApplication.kt
+++ app/src/main/java/com/ninebx/NineBxApplication.kt
@@ -90,7 +90,7 @@ class NineBxApplication : MultiDexApplication() {
             return nineBxPreferences!!
         }
 
-        var autoTestMode = false
+        var autoTestMode = true
 
         private var getUserAPI : NetModule.GetUsersAPI ?= null
         fun getUserAPI( ) : NetModule.GetUsersAPI? {
