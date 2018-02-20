9ce10a96dba053be2f5b3559c7147090994a72e8
Alok Omkar <alokomkar.gudikote@gmail.com>
2018-02-20 17:59:06 +0530

    Notifications fetched

--- app/src/main/java/com/ninebx/ui/home/notifications/NotificationsFragment.kt
+++ app/src/main/java/com/ninebx/ui/home/notifications/NotificationsFragment.kt
@@ -7,6 +7,9 @@ import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
 import com.ninebx.R
+import com.ninebx.ui.base.kotlin.hide
+import com.ninebx.ui.base.kotlin.show
+import com.ninebx.ui.base.kotlin.showToast
 import com.ninebx.ui.base.realm.decrypted.DecryptedNotifications
 import com.ninebx.ui.home.BaseHomeFragment
 import kotlinx.android.synthetic.main.fragment_notifications.*
@@ -15,19 +18,27 @@ import kotlinx.android.synthetic.main.fragment_notifications.*
  * Created by Alok on 03/01/18.
  */
 class NotificationsFragment : BaseHomeFragment(), NotificationsView {
+
     private var decryptedNotifications = ArrayList<DecryptedNotifications>()
 
-      override fun showProgress(message: Int) {
+    override fun showProgress(message: Int) {
+        progressLayout.show()
     }
 
     override fun hideProgress() {
+        progressLayout.hide()
     }
 
     override fun onError(error: Int) {
+        hideProgress()
+        context!!.showToast(error)
     }
 
     override fun onNotificationsFetched(notifications: ArrayList<DecryptedNotifications>) {
+        hideProgress()
         this.decryptedNotifications = notifications
+        rvNotification.layoutManager = LinearLayoutManager(context)
+        rvNotification.adapter = NotificationAdapter(decryptedNotifications)
     }
 
     override fun onCreateView(inflater: LayoutInflater, container: ViewGroup?, savedInstanceState: Bundle?): View? {
@@ -36,10 +47,6 @@ class NotificationsFragment : BaseHomeFragment(), NotificationsView {
 
     override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
         super.onViewCreated(view, savedInstanceState)
-
         NotificationsPresenter(this)
-
-        rvNotification.layoutManager = LinearLayoutManager(context)
-        rvNotification.adapter = NotificationAdapter(decryptedNotifications)
     }
 }
