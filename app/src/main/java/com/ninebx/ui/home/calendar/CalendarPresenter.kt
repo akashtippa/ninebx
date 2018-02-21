0b9c9af44da909ff9036ea571bdbae0f2608184d
Alok Omkar <alokomkar.gudikote@gmail.com>
2018-02-21 10:27:30 +0530

    Calendar events filtering in progress

--- app/src/main/java/com/ninebx/ui/home/calendar/CalendarPresenter.kt
+++ app/src/main/java/com/ninebx/ui/home/calendar/CalendarPresenter.kt
@@ -1,55 +1,74 @@
 package com.ninebx.ui.home.calendar
 
+import com.ninebx.R
 import com.ninebx.ui.base.realm.CalendarEvents
-import com.ninebx.ui.base.realm.Notifications
-import com.ninebx.ui.base.realm.decrypted.DecryptedNotifications
-import com.ninebx.utility.AppLogger
-import com.ninebx.utility.decryptNotifications
+import com.ninebx.utility.parseDateMonthYearFormat
+import com.ninebx.utility.parseDateMonthYearTimeFormat
 import com.ninebx.utility.prepareRealmConnections
 import io.realm.Realm
-import io.realm.RealmList
+import io.realm.RealmResults
 import io.realm.internal.SyncObjectServerFacade.getApplicationContext
+import java.util.*
+import kotlin.collections.ArrayList
+import kotlin.collections.HashMap
 
 /**
  * Created by Alok on 03/01/18.
  */
 class CalendarPresenter( val calendarView: CalendarView)  {
     private val context = getApplicationContext()
-    private val calendarEventsMap = HashMap<String, ArrayList<CalendarEvents>>()
+    private lateinit var calendarEventsMap : HashMap<Int, ArrayList<CalendarEvents>>
+
+    private var calendarEvents: RealmResults<CalendarEvents>? = null
+    private var calendarRealm: Realm? = null
 
     init {
+        calendarView.showProgress(R.string.loading)
         prepareRealmConnections(context, false, "CalendarEvents", object : Realm.Callback(){
             override fun onSuccess(realm: Realm?) {
-                val calendarEvents = realm!!.where(CalendarEvents::class.java).findAll()
+                calendarRealm = realm
 
-                var getCalendarItems = RealmList<String>()
-                if (calendarEvents.size > 0){
-                    for (i in 0 until calendarEvents.size) {
-                        getCalendarItems.add("ClassType : " + calendarEvents[i]!!.classType)
-                        getCalendarItems.add("Event ID : " + calendarEvents[i]!!.eventID)
-                        getCalendarItems.add("Title : " + calendarEvents[i]!!.title)
-                        getCalendarItems.add("Location : " + calendarEvents[i]!!.location)
-                        getCalendarItems.add("IsAllDay : " + calendarEvents[i]!!.isAllDay)
-                        getCalendarItems.add("Notes : " + calendarEvents[i]!!.notes)
-                        getCalendarItems.add("StartDate : " + calendarEvents[i]!!.startsDate)
-                        getCalendarItems.add("EndDate : " + calendarEvents[i]!!.endsDate)
-                        getCalendarItems.add("Repeat : " + calendarEvents[i]!!.repeats)
-                        getCalendarItems.add("EndRepeat : " + calendarEvents[i]!!.endRepeat)
-                        getCalendarItems.add("TravelTime : " + calendarEvents[i]!!.travelTime)
-                        getCalendarItems.add("Remainder : " + calendarEvents[i]!!.reminder)
-                        getCalendarItems.add("Alert : " + calendarEvents[i]!!.alert)
-                        getCalendarItems.add("ShowAs : " + calendarEvents[i]!!.showAs)
-                        getCalendarItems.add("URL : " + calendarEvents[i]!!.url)
-                        getCalendarItems.add("AttachmentNames : " + calendarEvents[i]!!)
-                        getCalendarItems.add("BackingImages : " + calendarEvents[i]!!)
-                        getCalendarItems.add("PhotosID : " + calendarEvents[i]!!)
-                    }
-                AppLogger.d("CalendarEvents", "Calendar events fetched" + getCalendarItems)
-            }
-                else{
-                    AppLogger.d("CalendarEvents", "calendar events not available")
-                }
+
+                calendarView.hideProgress()
             }
         })
+    }
+
+    fun filterEventsForMonth( monthString : String ) : HashMap<Int, ArrayList<CalendarEvents>> {
+        calendarEvents = calendarRealm!!.where(CalendarEvents::class.java)
+                .beginGroup()
+                .contains("startsDate", monthString )
+                .endGroup()
+                .findAll()
+
+        calendarEventsMap = HashMap()
+        return addEventToMap( calendarEvents )
+    }
+
+    private fun addEventToMap(events: RealmResults<CalendarEvents>?): HashMap<Int, ArrayList<CalendarEvents>> {
+        for( calendarEvent in  events!! ) {
+            for( startDate in calendarEvent.startsDate ) {
+                val parsedDate = parseAnyDate( startDate )
+                val calendar = Calendar.getInstance()
+                calendar.timeInMillis = parsedDate.time
+
+                if( !calendarEventsMap.containsKey(calendar.get(Calendar.DATE)) )
+                    calendarEventsMap.put(calendar.get(Calendar.DATE), ArrayList())
+
+                val eventsList = calendarEventsMap.get(calendar.get(Calendar.DATE))
+                eventsList!!.add(calendarEvent)
+            }
+
+        }
+        return calendarEventsMap
+    }
+
+    private fun parseAnyDate(startDate: String?): Date {
+        try {
+            return parseDateMonthYearFormat(startDate!!)
+        } catch ( e : Exception ) {
+            return parseDateMonthYearTimeFormat(startDate!!)
+        }
+
     }
 }
