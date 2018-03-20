9a880bd0c2a6962131b861daeaec0a015e8578bb
Alok Omkar <alokomkar.gudikote@gmail.com>
2018-03-20 19:23:31 +0530

    Add persons fix

--- app/src/main/java/com/ninebx/ui/home/baseCategories/CategoryItemClickListener.kt
+++ app/src/main/java/com/ninebx/ui/home/baseCategories/CategoryItemClickListener.kt
@@ -4,5 +4,5 @@ package com.ninebx.ui.home.baseCategories
  * Created by Alok on 12/01/18.
  */
 interface CategoryItemClickListener {
-    fun onItemClick( category: SubCategory, action : String )
+    fun onItemClick( mainCategory : Category, category: SubCategory, action : String )
 }
