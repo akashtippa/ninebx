e7b5766da92299df2dd4d0d4ac25a76d9544a40c
Alok Omkar <alokomkar.gudikote@gmail.com>
2018-03-22 12:31:17 +0530

    Setting level 3 title

--- app/src/main/java/com/ninebx/ui/home/baseSubCategories/Level3CategoryFragment.kt
+++ app/src/main/java/com/ninebx/ui/home/baseSubCategories/Level3CategoryFragment.kt
@@ -585,139 +585,139 @@ class Level3CategoryFragment : FragmentBackHelper(), Level2CategoryView {
             "Loans/Mortgages"-> {
                 etTitle.hint = "Institution name"
                 etTitleValue.hint = "Account name"
-                toolbarTitle.text = "Add Account"
+                if( selectedDocument == null ) toolbarTitle.text = "Add Account"
             }
             "Banking" -> {
                 etTitle.hint = "Institution name"
                 etTitleValue.hint = "Account name"
-                toolbarTitle.text = "Add Account"
+                if( selectedDocument == null ) toolbarTitle.text = "Add Account"
             }
             "Investments/Retirement" -> {
                 etTitle.hint = "Institution name"
                 etTitleValue.hint = "Account name"
-                toolbarTitle.text = "Add Account"
+                if( selectedDocument == null ) toolbarTitle.text = "Add Account"
             }
             "Loan/Mortgages" -> {
                 etTitle.hint = "Institution name"
                 etTitleValue.hint = "Account name"
-                toolbarTitle.text = "Add Account"
+                if( selectedDocument == null ) toolbarTitle.text = "Add Account"
             }
             "Other financial accounts" -> {
                 etTitle.hint = "Institution name"
                 etTitleValue.hint = "Account name"
-                toolbarTitle.text = "Add Account"
+                if( selectedDocument == null ) toolbarTitle.text = "Add Account"
             }
 
         // Payment Methods
             "Credit/Debit cards" -> {
                 etTitle.hint = "Card name"
                 etTitleValue.hint = ""
-                toolbarTitle.text = "Add Account"
+                if( selectedDocument == null ) toolbarTitle.text = "Add Account"
             }
 
             "Other payment accounts" -> {
                 etTitle.hint = "Institution name"
                 etTitleValue.hint = "Account name"
-                toolbarTitle.text = "Add Account"
+                if( selectedDocument == null ) toolbarTitle.text = "Add Account"
             }
 
         // Property
             "Primary home (owned)" -> {
                 etTitleValue.hint = ""
                 etTitle.hint = "Property name"
-                toolbarTitle.text = "Add Property"
+                if( selectedDocument == null ) toolbarTitle.text = "Add Property"
 
             }
             "Property (rented for own use)" -> {
                 etTitleValue.hint = ""
                 etTitle.hint = "Property name"
-                toolbarTitle.text = "Add Account"
+                if( selectedDocument == null ) toolbarTitle.text = "Add Account"
 
             }
             "Vacation home" -> {
                 etTitle.hint = "Property name"
                 etTitleValue.hint = ""
-                toolbarTitle.text = "Add Account"
+                if( selectedDocument == null ) toolbarTitle.text = "Add Account"
             }
             "Investment/Rental property" -> {
                 etTitle.hint = "Property name"
                 etTitleValue.hint = ""
-                toolbarTitle.text = "Add Account"
+                if( selectedDocument == null ) toolbarTitle.text = "Add Account"
             }
 
         // Auto
             "Vehicles" -> {
                 etTitle.hint = "Vehicle name"
                 etTitleValue.hint = "License plate number"
-                toolbarTitle.text = "Add Vehicle"
+                if( selectedDocument == null ) toolbarTitle.text = "Add Vehicle"
             }
 
             "Maintenance" -> {
                 etTitle.hint = "Maintenance event"
                 etTitleValue.hint = "Vehicle"
-                toolbarTitle.text = "Add Record"
+                if( selectedDocument == null ) toolbarTitle.text = "Add Record"
             }
 
         // Other Assets
             "Jewelry" -> {
                 etTitle.hint = "Asset name"
                 etTitleValue.hint = "Description/location"
-                toolbarTitle.text = "Add Asset"
+                if( selectedDocument == null ) toolbarTitle.text = "Add Asset"
             }
 
             "Art and collectibles" -> {
                 etTitle.hint = "Asset name"
                 etTitleValue.hint = "Description/location"
-                toolbarTitle.text = "Add Asset"
+                if( selectedDocument == null ) toolbarTitle.text = "Add Asset"
             }
 
             "Computers and electronics" -> {
                 etTitle.hint = "Asset name"
                 etTitleValue.hint = "Description/location"
-                toolbarTitle.text = "Add Asset"
+                if( selectedDocument == null ) toolbarTitle.text = "Add Asset"
             }
 
             "Furniture" -> {
                 etTitle.hint = "Asset name"
                 etTitleValue.hint = "Description/location"
-                toolbarTitle.text = "Add Asset"
+                if( selectedDocument == null ) toolbarTitle.text = "Add Asset"
             }
 
             "Others" -> {
                 etTitle.hint = "Asset name"
                 etTitleValue.hint = "Description/location"
-                toolbarTitle.text = "Add Asset"
+                if( selectedDocument == null ) toolbarTitle.text = "Add Asset"
             }
 
         // Insurance
             "Homeowners/Renters insurance" -> {
                 etTitle.hint = "Insurance company"
                 etTitleValue.hint = "Insured property"
-                toolbarTitle.text = "Add Policy"
+                if( selectedDocument == null ) toolbarTitle.text = "Add Policy"
             }
 
             "Auto insurance" -> {
                 etTitle.hint = "Insurance company"
                 etTitleValue.hint = "Insured vehicle"
-                toolbarTitle.text = "Add Policy"
+                if( selectedDocument == null ) toolbarTitle.text = "Add Policy"
             }
 
             "Life insurance" -> {
                 etTitle.hint = "Insurance company"
                 etTitleValue.hint = "Insured person"
-                toolbarTitle.text = "Add Policy"
+                if( selectedDocument == null ) toolbarTitle.text = "Add Policy"
             }
 
             "Health insurance" -> {
                 etTitle.hint = "Insurance company"
                 etTitleValue.hint = "Insured person"
-                toolbarTitle.text = "Add Policy"
+                if( selectedDocument == null ) toolbarTitle.text = "Add Policy"
             }
 
             "Umbrella insurance" -> {
                 etTitle.hint = "Insurance company"
                 etTitleValue.hint = "Insured person"
-                toolbarTitle.text = "Add Policy"
+                if( selectedDocument == null ) toolbarTitle.text = "Add Policy"
             }
 
         // Taxes
@@ -725,54 +725,54 @@ class Level3CategoryFragment : FragmentBackHelper(), Level2CategoryView {
             "Past returns" -> {
                 etTitle.hint = "Return name"
                 etTitleValue.hint = ""
-                toolbarTitle.text = "Add Return"
+                if( selectedDocument == null ) toolbarTitle.text = "Add Return"
             }
 
             "Returns to be filed" -> {
                 etTitle.hint = "Return name"
                 etTitleValue.hint = ""
-                toolbarTitle.text = "Add Return"
+                if( selectedDocument == null ) toolbarTitle.text = "Add Return"
             }
 
         // Travel
             "Airline" -> {
                 etTitle.hint = "Airline"
                 etTitleValue.hint = "Account name"
-                toolbarTitle.text = "Add Account"
+                if( selectedDocument == null ) toolbarTitle.text = "Add Account"
             }
 
             "Hotel" -> {
                 etTitle.hint = "Hotel"
                 etTitleValue.hint = "Account name"
-                toolbarTitle.text = "Add Account"
+                if( selectedDocument == null ) toolbarTitle.text = "Add Account"
 
             }
 
             "Car Rental" -> {
                 etTitle.hint = "Car Rental Company"
                 etTitleValue.hint = "Account name"
-                toolbarTitle.text = "Add Account"
+                if( selectedDocument == null ) toolbarTitle.text = "Add Account"
 
             }
 
             "Cruiseline" -> {
                 etTitle.hint = "Cruiseline"
                 etTitleValue.hint = "Account name"
-                toolbarTitle.text = "Add Account"
+                if( selectedDocument == null ) toolbarTitle.text = "Add Account"
 
             }
 
             "Railway" -> {
                 etTitle.hint = "Railway"
                 etTitleValue.hint = "Account name"
-                toolbarTitle.text = "Add Account"
+                if( selectedDocument == null ) toolbarTitle.text = "Add Account"
 
             }
 
             "Other" -> {
                 etTitle.hint = "Other"
                 etTitleValue.hint = "Account name"
-                toolbarTitle.text = "Add Account"
+                if( selectedDocument == null ) toolbarTitle.text = "Add Account"
 
             }
 
@@ -780,21 +780,21 @@ class Level3CategoryFragment : FragmentBackHelper(), Level2CategoryView {
                 etTitle.hint = "Passport name"
                 etTitleValue.hint = ""
                 etTitleValue.setEnabled(false)
-                toolbarTitle.text = "Add Passport"
+                if( selectedDocument == null ) toolbarTitle.text = "Add Passport"
             }
 
             "Visa" -> {
                 etTitle.hint = "Visa name"
                 etTitleValue.hint = ""
                 etTitleValue.setEnabled(false)
-                toolbarTitle.text = "Add Visa"
+                if( selectedDocument == null ) toolbarTitle.text = "Add Visa"
             }
 
             "Other travel document" -> {
                 etTitle.hint = "Travel document title"
                 etTitleValue.hint = ""
                 etTitleValue.setEnabled(false)
-                toolbarTitle.text = "Add Document"
+                if( selectedDocument == null ) toolbarTitle.text = "Add Document"
 
             }
 
@@ -802,20 +802,20 @@ class Level3CategoryFragment : FragmentBackHelper(), Level2CategoryView {
                 etTitle.hint = "Description"
                 etTitleValue.hint = ""
                 etTitleValue.setEnabled(false)
-                toolbarTitle.text = "Add Travel Plan"
+                if( selectedDocument == null ) toolbarTitle.text = "Add Travel Plan"
 
             }
 
             "Services/Other Accounts" -> {
                 etTitle.hint = "Institution name"
                 etTitleValue.hint = "Account name"
-                toolbarTitle.text = "Add Account"
+                if( selectedDocument == null ) toolbarTitle.text = "Add Account"
             }
 
             "Other Attachments" -> {
                 etTitle.hint = "Title"
                 etTitleValue.hint = ""
-                toolbarTitle.text = "Add Attachment"
+                if( selectedDocument == null ) toolbarTitle.text = "Add Attachment"
             }
 
         // Personal
@@ -824,113 +824,113 @@ class Level3CategoryFragment : FragmentBackHelper(), Level2CategoryView {
                 etTitleValue.hint = ""
                 etTitleValue.setEnabled(false)
 
-                toolbarTitle.text = "Add License"
+                if( selectedDocument == null ) toolbarTitle.text = "Add License"
             }
             "Social Security Card" -> {
                 etTitle.hint = "Card name"
                 etTitleValue.hint = ""
                 etTitleValue.setEnabled(false)
 
-                toolbarTitle.text = "Add Card"
+                if( selectedDocument == null ) toolbarTitle.text = "Add Card"
             }
             "Tax ID" -> {
                 etTitle.hint = "Tax ID name"
                 etTitleValue.hint = ""
                 etTitleValue.setEnabled(false)
 
-                toolbarTitle.text = "Add ID"
+                if( selectedDocument == null ) toolbarTitle.text = "Add ID"
             }
             "Birth Certificate" -> {
                 etTitle.hint = "Description"
                 etTitleValue.hint = ""
                 etTitleValue.isEnabled = false
 
-                toolbarTitle.text = "Add Certificate"
+                if( selectedDocument == null ) toolbarTitle.text = "Add Certificate"
             }
             "Marriage Certificate" -> {
                 etTitle.hint = "Description"
                 etTitleValue.hint = ""
                 etTitleValue.isEnabled = false
 
-                toolbarTitle.text = "Add Certificate"
+                if( selectedDocument == null ) toolbarTitle.text = "Add Certificate"
             }
             "Other Government-Issued ID" -> {
                 etTitle.hint = "ID name"
                 etTitleValue.hint = ""
                 etTitleValue.setEnabled(false)
 
-                toolbarTitle.text = "Add ID"
+                if( selectedDocument == null ) toolbarTitle.text = "Add ID"
             }
 
             "Add Person" -> {
                 etTitle.hint = "Institution name"
                 etTitleValue.hint = "Qualification/degree"
-                toolbarTitle.text = "Add Institution"
+                if( selectedDocument == null ) toolbarTitle.text = "Add Institution"
             }
             "Add person" -> {
                 etTitle.hint = "Company name"
                 etTitleValue.hint = "Position"
-                toolbarTitle.text = "Add Company"
+                if( selectedDocument == null ) toolbarTitle.text = "Add Company"
             }
 
             "Loyalty Programs" -> {
                 etTitle.hint = "Brand/Retailer"
                 etTitleValue.hint = "Account name"
-                toolbarTitle.text = "Add Account"
+                if( selectedDocument == null ) toolbarTitle.text = "Add Account"
 
             }
 
             "Recent Purchases" -> {
                 etTitleValue.hint = "Brand/Retailer"
                 etTitle.hint = "Item"
-                toolbarTitle.text = "Add Purchase"
+                if( selectedDocument == null ) toolbarTitle.text = "Add Purchase"
             }
 
             "Womens sizes" -> {
                 etTitle.hint = "User name"
                 etTitleValue.hint = "Size name"
-                toolbarTitle.text = "Add Women's sizes"
+                if( selectedDocument == null ) toolbarTitle.text = "Add Women's sizes"
             }
 
             "Mens sizes" -> {
                 etTitle.hint = "User name"
                 etTitleValue.hint = "Size name"
-                toolbarTitle.text = "Add Mens's sizes"
+                if( selectedDocument == null ) toolbarTitle.text = "Add Mens's sizes"
             }
 
             "Girls sizes" -> {
                 etTitle.hint = "User name"
                 etTitleValue.hint = "Size name"
-                toolbarTitle.text = "Add Girls's sizes"
+                if( selectedDocument == null ) toolbarTitle.text = "Add Girls's sizes"
             }
             "Boy's sizes" -> {
                 etTitle.hint = "User name"
                 etTitleValue.hint = "Size name"
-                toolbarTitle.text = "Add Boy's sizes"
+                if( selectedDocument == null ) toolbarTitle.text = "Add Boy's sizes"
             }
 
             "Baby's sizes" -> {
                 etTitle.hint = "User name"
                 etTitleValue.hint = "Size name"
-                toolbarTitle.text = "Add Baby's sizes"
+                if( selectedDocument == null ) toolbarTitle.text = "Add Baby's sizes"
             }
 
             "Identification" -> {
                 etTitle.hint = "User name"
                 etTitleValue.hint = ""
-                toolbarTitle.text = "Add Identification"
+                if( selectedDocument == null ) toolbarTitle.text = "Add Identification"
             }
 
             "Medical history" -> {
                 etTitle.hint = "History"
                 etTitleValue.hint = ""
-                toolbarTitle.text = "Add History"
+                if( selectedDocument == null ) toolbarTitle.text = "Add History"
             }
 
             "Healthcare providers" -> {
                 etTitle.hint = "Name"
                 etTitleValue.hint = "Type of physician"
-                toolbarTitle.text = "Add Providers"
+                if( selectedDocument == null ) toolbarTitle.text = "Add Providers"
             }
 
             "Emergency contacts" -> {
@@ -940,31 +940,31 @@ class Level3CategoryFragment : FragmentBackHelper(), Level2CategoryView {
             "Medications" -> {
                 etTitle.hint = "Name"
                 etTitleValue.hint = "Dose/strength"
-                toolbarTitle.text = "Add Medications"
+                if( selectedDocument == null ) toolbarTitle.text = "Add Medications"
             }
 
             "Medical conditions/Allergies" -> {
                 etTitle.hint = "Condition"
                 etTitleValue.hint = "Date diagnosed"
-                toolbarTitle.text = "Add Conditions"
+                if( selectedDocument == null ) toolbarTitle.text = "Add Conditions"
             }
 
             "Eyeglass prescriptions" -> {
                 etTitle.hint = "Physician name"
                 etTitleValue.hint = "Date prescribed"
-                toolbarTitle.text = "Add Prescription"
+                if( selectedDocument == null ) toolbarTitle.text = "Add Prescription"
             }
 
             "Vital numbers" -> {
                 etTitle.hint = "Description"
                 etTitleValue.hint = "Measurement date"
-                toolbarTitle.text = "Add Record"
+                if( selectedDocument == null ) toolbarTitle.text = "Add Record"
             }
 
             "Checkups and visits" -> {
                 etTitle.hint = "Description"
                 etTitleValue.hint = "Physician name"
-                toolbarTitle.text = "Add Visit"
+                if( selectedDocument == null ) toolbarTitle.text = "Add Visit"
             }
         }
     }
